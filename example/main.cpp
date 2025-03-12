940fe97dab89423705c24ab758f78f5faaed89c5
Teo Fumagalli <teo.fum@gmail.com>
2025-03-12 11:15:20 -0300

    rm unused funcs from example

--- example/main.cpp
+++ example/main.cpp
@@ -3,17 +3,6 @@
 // IEEE-754 half-precision float to 32-bit float conversion
 // https://stackoverflow.com/a/60047308
 static uint32_t as_uint(const float x) { return *(uint32_t*) &x; }
-static float as_float(const uint32_t x) { return *(float*) &x; }
-
-static float half_to_float(const uint16_t x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
-  const uint32_t e = (x & 0x7C00) >> 10; // exponent
-  const uint32_t m = (x & 0x03FF) << 13; // mantissa
-  const uint32_t v = as_uint((float) m) >> 23; // evil log2 bit hack to count leading zeros in denormalized format
-  return as_float((x & 0x8000) << 16
-                  | (e != 0) * ((e + 112) << 23 | m)
-                  | ((e == 0) & (m != 0)) *
-                    ((v - 37) << 23 | ((m << (150 - v)) & 0x007FE000))); // sign : normalized : denormalized
-}
 uint16_t float_to_half(const float x) { // IEEE-754 16-bit floating-point format (without infinity): 1-5-10, exp-15, +-131008.0, +-6.1035156E-5, +-5.9604645E-8, 3.311 digits
   const uint32_t b = as_uint(x) + 0x00001000; // round-to-nearest-even: add last bit after truncated mantissa
   const uint32_t e = (b & 0x7F800000) >> 23; // exponent
